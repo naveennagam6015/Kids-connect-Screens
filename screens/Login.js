68489c58f32419978ca4e361ae66596577081207
Soumya7077 <110816458+Soumya7077@users.noreply.github.com>
2024-03-04 17:02:38 +0530

    Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { tokens } from "../assets/colors/theme";
+import { TextBold } from "../assets/fonts/CustomText";
+
 
 
 export default function Login(){
     return(
         <View style={styles.container}>
-            <Text style={styles.heading}>kids Connect</Text>
+            <TextBold style={styles.heading}>Kids Connect</TextBold>
+            <TextBold style={styles.subheading}>Welcome back</TextBold>
+            <View>
+                <TextInput style={styles.inputBox} placeholder="Enter your mail" />
+                <TextInput style={styles.inputBox} placeholder="Enter your password" />
+            </View>
         </View>
     )
 }
@@ -13,12 +20,24 @@ export default function Login(){
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        padding:10,
+        padding:15,
         justifyContent:'center'
     },
     heading:{
         fontSize:60,
         alignSelf:'center',
         color:tokens.neutral[700]
+    },
+    subheading:{
+        fontSize:25,
+        alignSelf:'center',
+        color:tokens.neutral[500]
+    },
+    inputBox:{
+        padding:10,
+        borderColor:tokens.neutral[500],
+        borderWidth:1,
+        margin:12,
+        borderRadius:8
     }
 })
