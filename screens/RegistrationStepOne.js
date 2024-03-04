b09ffd933028bbe88eff311909f83531bf6d3e56
unknown <naveennagam6015@gmail.com>
2024-03-04 17:02:56 +0530

    RegistratinStepOne

---
+++ screens/RegistrationStepOne.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import CustomText from '../assets/Fonts/CustomText.js';
+
+export default function RegistrationStepOne() {
+    return (
+        <View>
+            <TextBold>Enter your mail
+            </TextBold>
+        </View>
+    )
+}
