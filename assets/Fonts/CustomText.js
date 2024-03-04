b1c86d40aec87b580ceb138ff1a701970430a506
unknown <naveennagam6015@gmail.com>
2024-03-04 16:34:58 +0530

    Customtext

--- assets/Fonts/CustomText.js
+++ assets/Fonts/CustomText.js
@@ -2,18 +2,48 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const CustomText = ({ children, style, ...rest }) => {
+const TextRegular = ({ children, style, ...rest }) => {
     return (
-        <Text style={[styles.text, style]} {...rest}>
+        <Text style={[styles.TextRegular, style]} {...rest}>
+            {children}
+        </Text>
+    );
+};
+const TextLight = ({ children, style, ...rest }) => {
+    return (
+        <Text style={[styles.TextLight, style]} {...rest}>
+            {children}
+        </Text>
+    );
+};
+const TextMedium = ({ children, style, ...rest }) => {
+    return (
+        <Text style={[styles.TextMedium, style]} {...rest}>
+            {children}
+        </Text>
+    );
+};
+const TextBold = ({ children, style, ...rest }) => {
+    return (
+        <Text style={[styles.TextBold, style]} {...rest}>
             {children}
         </Text>
     );
 };
 
 const styles = StyleSheet.create({
-    text: {
-        fontFamily: "LexendRegular",
+    TextRegular: {
+        fontFamily: "RobotoRegular",
+    },
+    TextLight: {
+        fontFamily: "RobotoLight",
+    },
+    TextMedium: {
+        fontFamily: "RobotoMedium",
+    },
+    TextBold: {
+        fontFamily: "RobotoMedium",
     },
 });
 
-export default CustomText;
+export default (TextRegular, TextLight, TextMedium, TextBold);
