a5a18eb722cff7a6b458fdc803423e42c7aa3039
unknown <naveennagam6015@gmail.com>
2024-04-01 11:01:14 +0530

    Test

--- Test.js
+++ Test.js
@@ -1,42 +1,21 @@
 
 
-import React, { useState } from 'react'
-import { StyleSheet, View, Modal, } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { TextRegular } from './assets/fonts/CustomText'
 import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native';
-import { color } from './assets/colors/theme';
 
 export default function Test() {
     const navigation = useNavigation();
-    const [ModalVisible, setModalVisible] = useState(false);
 
     return (
-
         <View style={[styles.container, { flexDirection: 'column', },]}>
-            <Button title='Open' onPress={() => { setModalVisible(true); }} />
-
             <TextRegular style={[styles.fontsize]}> ViewPropTypes will be removed from React Native, along with all other PropTypes. We recommend that you migrate away
                 from PropTypes and switch to a type system like TypeScript. If you need to continue using ViewPropTypes, migrate to the 'deprecated-react-native-prop-types' package.
             </TextRegular>
             <TextRegular style={[styles.fontsize]}> ViewPropTypes will be removed from React Native, along with all other PropTypes. We recommend that you migrate away
                 from PropTypes and switch to a type system like TypeScript. If you need to continue using ViewPropTypes, migrate to the 'deprecated-react-native-prop-types' package.
             </TextRegular>
-            <Modal
-                animationType="slide"
-                visible={ModalVisible}
-                backdropTransitionOutTiming={1000}
-                useNativeDriver={true}
-                transparent={true}>
-                <View style={styles.topDummy} />
-                <View style={styles.bottomMain} >
-                    <Button title='Close' onPress={() => { setModalVisible(false); }} />
-                </View>
-            </Modal>
         </View>
-
-
-
     );
 
 }
