61a064b1503844d5378e8dd5d171d5fdf74f24bc
unknown <naveennagam6015@gmail.com>
2024-04-05 16:43:36 +0530

    Modal

--- screens/ProfileEdit.js
+++ screens/ProfileEdit.js
@@ -13,11 +13,6 @@ export default function ProfileEdit() {
     const [year, setYear] = useState('');
     const [notifications, setNotifications] = useState(false);
     const [darkmode, setDarkmode] = useState(false);
-    // const [year, setYear] = useState(''); const [relationship, setRelationship] = useState([
-    //     { label: 'Mother', value: '1' },
-    //     { label: 'Father', value: '2' },
-    // ]);
-
 
     useEffect(() => {
         async function GetUserData() {
@@ -35,12 +30,126 @@ export default function ProfileEdit() {
 
         GetUserData();
     }, [])
+
+    /*=============================================Camera Permission========================================*/
+
+    useEffect(() => {
+        (async () => {
+            const galleryStatus =
+                await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryImagePermission(galleryStatus.status === "granted");
+        })();
+        loadImages();
+    }, []);
+
+    const loadImages = async () => {
+        await ensureDirExists();
+        const files = await FileSystem.readDirectoryAsync(imgDir);
+        if (files.length > 0) {
+            setImage(files.map((f) => imgDir + f));
+        }
+    };
+
+    const OpenCamera = async (useLibrary) => {
+        let result;
+        // if (!useLibrary) {
+        await ImagePicker.getCameraPermissionsAsync();
+
+        result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 1,
+        });
+        // }
+
+        if (!result.canceled) {
+            // Extracting the filename from the local URI
+            const filename =
+                Platform.OS === "android"
+                    ? result.assets[0].uri.split("/").pop()
+                    : result.assets[0].uri.split("/").pop().split(".")[0];
+
+            // console.log(result.assets[0].uri);
+            // setImage(result.assets[0].uri);
+            console.log(result);
+            saveImage(result.assets[0].uri);
+        }
+    };
+
+    /*==================================================Camera permission functionality end========================================= */
+
+    /*===================================================Upload Image functionality==================================================== */
+
+    const saveImage = async (uri) => {
+        await ensureDirExists();
+        const fileName = new Date().getTime() + ".jpg";
+        const dest = imgDir + fileName;
+        await FileSystem.copyAsync({ from: uri, to: dest });
+        setImage([...image, dest]);
+
+        try {
+            const fileName = uri.split("/").pop();
+            const base64Credentials = base64Encode(
+                "glansaapi:MB9j xRKL jJSX CtmR nAC3 XBlL"
+            );
+            const formData = new FormData();
+            formData.append("file", {
+                uri: uri,
+                name: fileName,
+                type: "image/jpeg",
+            });
+
+            setButtonLoading(true);
+            // setGalleryLoading(true)
+            const response = await fetch(`${BASEURL}wp-json/wp/v2/media`, {
+                method: "POST",
+                headers: {
+                    Authorization: `Basic ${base64Credentials}`,
+                    Accept: "application/json",
+                    "Content-Type": "multipart/form-data",
+                },
+                body: formData,
+            });
+
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log("Upload to WordPress successful:", responseData);
+                console.log(responseData.source_url);
+                if (isPickingImage) {
+                    setUploadImage((prevUploadImage) => [
+                        ...prevUploadImage,
+                        responseData.source_url,
+                    ]);
+                } else {
+                    setGalleryImages((prevUploadImage) => [
+                        ...prevUploadImage,
+                        responseData.source_url,
+                    ]);
+                }
+            } else {
+                console.error(
+                    "Upload to WordPress failed with status:",
+                    response.status
+                );
+            }
+        } catch (error) {
+            console.error("Error uploading image to WordPress:", error);
+        } finally {
+            setButtonLoading(false);
+            // setGalleryLoading(false);
+            setIsPickingImage(false);
+        }
+    };
+
+    /*========================================================Image Upload Functionality end====================================== */
+
+
     return (
         <ScrollView style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
                 <View style={{ alignItems: 'center' }}>
                     <View>
-                        <Image style={[styles.profilepic]} source={require('../assets/images/women.png')} />
+                        <Image style={[styles.profilepic]} source={{ uri: userData.ProfileImage }} />
                     </View>
                 </View>
                 <TextBold>About</TextBold>
@@ -77,7 +186,7 @@ export default function ProfileEdit() {
                         }));
                     }}
                 />
-                <TextBold>Relationship</TextBold>
+                {/* <TextBold>Relationship</TextBold> */}
                 {/* <Dropdown
                     style={styles.dropdownStyle}
                     data={relationship}
@@ -104,9 +213,6 @@ export default function ProfileEdit() {
                         }));
                     }}
                 />
-                <TextBold>Kids connect since</TextBold>
-                <TextBold style={{ color: color.neutral[300], fontSize: 12 }}>(Since {month} {year})</TextBold>
-
                 <View style={[styles.modalcontainer]}>
                     <View style={[styles.flexrow, { justifyContent: 'space-between' }]}>
                         <TouchableOpacity style={[styles.Buttoncardinner, styles.Buttoncardwidth,]}>
@@ -117,6 +223,7 @@ export default function ProfileEdit() {
                         </TouchableOpacity>
                     </View>
                 </View>
+
             </SafeAreaView>
         </ScrollView>
     )
