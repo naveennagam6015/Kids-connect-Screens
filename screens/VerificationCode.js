c761f104850dbdf7bf85ec033cac9be98e917ba5
AnitaSeth1 <anita.glansa@gmail.com>
2024-05-01 12:01:29 +0530

    varification code

--- screens/VerificationCode.js
+++ screens/VerificationCode.js
@@ -1,101 +1,82 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, TextInput,TextBold,TextRegular } from 'react-native';
-// import { TouchableOpacity, Text } from 'react-native-gesture-handler';
+import { View, TouchableOpacity, Text, StyleSheet, TextInput, TextBold, TextRegular, KeyboardAvoidingView } from 'react-native';
 import { color } from "../assets/colors/theme";
 import { ImageBackground } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+
 export default function VerificationScreen() {
     const navigation = useNavigation();
 
-
     return (
-        <ImageBackground source={require('../assets/images/login.jpg')} style={{ width: '100%', height: '100%' }}>
+        <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+            <ImageBackground source={require('../assets/images/login.jpg')} style={{ width: '100%', height: '100%' }}>
+                <View style={styles.centeredContainer}>
+                    <View style={styles.container}>
+                        <Text>Enter the OTP sent to your email</Text>
+                        <View style={[styles.flexrow, styles.justiffsb]}>
+                            <View style={styles.inputfiled}></View>
+                            <View style={styles.inputfiled}></View>
+                            <View style={styles.inputfiled}></View>
+                            <View style={styles.inputfiled}></View>
+                        </View>
 
-        <View>
-            
-            <View style={styles.container}>
-                <Text>Enter the OTP sent to your email</Text>
-                <View style={[styles.flexrow, styles.justiffsb]}>
-               <View style={styles.inputfiled}>
-                 </View>
-               <View style={styles.inputfiled}>
-                 </View>
-               <View style={styles.inputfiled}>
-                 </View>
-               <View style={styles.inputfiled}>
-                 </View>
-                 
-              
-              </View>
+                        <TouchableOpacity onPress={() => navigation.navigate('Passwordsetup')} style={styles.btnPrimary}>
+                            <Text style={{ color: color.fontcolor, fontWeight: "bold", textAlign: 'center' }}>Verify <AntDesign name="arrowright" size={18} color="black" /></Text>
+                        </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Passwordsetup')} style={styles.btnPrimary}>
-                <Text style={{ color: color.fontcolor, fontWeight: "bold", textAlign: 'center' }}>Verify <AntDesign name="arrowright" size={18} color="black" /></Text>
-            </TouchableOpacity>
-            <View>
-        <Text>Your Unique Code is sent to the mail:alright@gmail.com,</Text>
-        <Text>This unique code will be used to signup</Text>
-      </View>
-        </View>
-      
-      
-  </View >
-  </ImageBackground>
-  );
-
+                        <View style={styles.infoTextContainer}>
+                            <Text style={styles.infoText}>Your Unique Code is sent to the mail: <Text style={{ color: 'grey' }}>alright@gmail.com</Text>,</Text>
+                            <Text style={styles.infoText}>This unique code will be used to signup</Text>
+                        </View>
+                    </View>
+                </View>
+            </ImageBackground>
+        </KeyboardAvoidingView>
+    );
 }
+
 const styles = StyleSheet.create({
-    container: {
-        padding: 15,
+    centeredContainer: {
+        flex: 1,
+        alignItems: "center",
         justifyContent: "center",
-        marginTop: 200,
-        marginLeft:30,
-        width:"90%",
-        height:"50%",
+    },
+    container: {
+        padding: 20,
         backgroundColor: color.neutral[100],
         borderRadius: 10,
-        alignItems: "center",
-        textAlign : "center"
-
-
     },
-    flexrow:{
+    flexrow: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
     },
     btnPrimary: {
         padding: 15,
-        margin: 1,
         backgroundColor: color.primary,
         borderRadius: 8,
         marginVertical: 50,
-        width:"80%"
     },
     inputfiled: {
         borderColor: color.primary,
         borderWidth: 1,
         marginBottom: 10,
-        width:"20%",
+        width: "20%",
         padding: 5,
         margin: 5,
-        backgroundColor:color.neutral[200],
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingVertical: 4,
-        paddingHorizontal: 8,
+        backgroundColor: color.neutral[200],
         borderRadius: 8,
-        marginTop: 4,
-        marginBottom: 8,
         height: 50,
-      },
-      inputBox: {
-        padding: 10,
-        borderColor: color.neutral[300],
-        borderWidth: 1,
-        borderRadius: 8,
-        marginTop: 8,
-        width: "100%",
-        marginBottom: 16,
-      },
-})
+    },
+    infoTextContainer: {
+        marginTop: 20,
+        alignItems: "center",
+    },
+    infoTextContainer: {
+        marginTop: 20,
+    },
+    infoText: {
+        color: color.fontcolor, // Change this to the grey color you want
+    },
+});
